634929d3b6f377a6375abd948c8566cb0ac253dc
Sashank Tadimeti <stadimeti3@gatech.edu>
2025-09-05 20:41:43 -0400

    Initial Commit

---
+++ MealPlanner/MealPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealPlannerApp.swift
+//  MealPlanner
+//
+//  Created by Sashank Tadimeti on 9/5/25.
+//
+
+import SwiftUI
+
+@main
+struct MealPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MealPlannerTests/MealPlannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MealPlannerTests.swift
+//  MealPlannerTests
+//
+//  Created by Sashank Tadimeti on 9/5/25.
+//
+
+import Testing
+@testable import MealPlanner
+
+struct MealPlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
